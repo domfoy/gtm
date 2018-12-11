a3c70f75ba36e3f8225051ae2f9e39e04a7c68ef
domfoy <quentin.pergeline@hotmail.fr>
2018-12-11 21:09:26 +0100

    feat(datamodel): reshapes the entry model

--- back/src/model/entry.js
+++ back/src/model/entry.js
@@ -5,6 +5,15 @@ const Types = Schema.Types;
 
 const schema = new Schema({
   ref: Types.String,
+  title: {
+    en: Types.String
+  },
+  direction: [Types.String],
+  cast: [Types.String],
+  tagLines: {
+    en: [Types.String]
+  },
+  year: Types.Number,
   updateCount: Types.Number,
   insertDate: Types.Date,
   updateDate: Types.Date,
